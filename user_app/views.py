e958ad9c5055cfb440d3ca9ce5166de1dda71a0b
Vaisakh P V <vaisakhpv033@gmail.com>
2024-08-17 19:13:39 +0530

    created user_app implemented login, logout, signup features working successfully

---
+++ user_app/views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import cache_control
+from .forms import LoginForm, SignupForm
+
+
+@login_required(login_url='user_login')
+@cache_control(no_cache=True, must_revalidate=True, no_store=True)
+def user_index(request):
+    return render(request, 'user/index.html')
+
+
+def user_login(request):
+    if request.user.is_authenticated:
+        return redirect('user_index')
+    
+    if request.method=='POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('user_index')
+            else:
+                context = {'form': form, 'message': 'Invalid Credentials'}
+                return render(request, 'user/login.html', context)
+    else:
+        form = LoginForm()
+    return render(request, 'user/login.html', {'form': form})
+
+
+@login_required(login_url='user_login')
+def user_logout(request):
+    logout(request)
+    return redirect('user_login')
+
+
+def user_signup(request):
+    if request.user.is_authenticated:
+        return redirect('user_index')
+    
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('user_login')
+    else:
+        form = SignupForm()
+    return render(request, 'user/signup.html', {'form': form})
